079f78986f23ad2e7e956ced4f185c2c636a726a
j <j@j.com>
2016-09-02 00:26:16 +1000

    Imgur api now sending to props.images. Still need to filter

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
-const IMAGES = [
-  {title: 'Pen',link: 'http://dummyimage.com/600x400'},
-  {title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
-  {title: 'Mug', link: 'http://dummyimage.com/600x400'}
-];
+// const IMAGES = [
+//   {title: 'Pen',link: 'http://dummyimage.com/600x400'},
+//   {title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
+//   {title: 'Mug', link: 'http://dummyimage.com/600x400'}
+// ];
 
 // Create image list component
-const ImageList = () => {
-  const RenderedImages = IMAGES.map(image =>
+const ImageList = (props) => {
+  const RenderedImages = props.images.map(image =>
     <ImageDetail key={image.title} image={image}/>
   );
 
--- client/main.js
+++ client/main.js
@@ -4,22 +4,49 @@ import ImageList from './components/image_list';
 import axios from 'axios';
 
 // create a component
-const App = () => {
-  return(
-      <div>
-        This is app
-        <ImageList />
-      </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    // Whenever an App is created,
+    // the constructor is created
+
+    // and call the constructor the Component extension
+    super(props);
+
+    this.state = {images: []};
+  }
+
+  componentWillMount(){
+    // FANTASTIC PLACE TO LOAD data
+    // because only called once
+
+    // axios
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => {
+        // we are interested in response.data.data
+        // instead of this.render()
+        this.setState({images: response.data.data});
+        console.log("Imgur data ready");
+        console.log(this.state.images);
+    });
+
+    console.log("App is about to render");
+  }
+
+  render(){
+
+    // now to make it visible on render, call the state
+    // console.log(this.state.images);
+    return(
+        <div>
+          This is app
+          <ImageList images={this.state.images}/>
+        </div>
+    );
+  }
 };
 
 
 Meteor.startup(() =>{
   ReactDOM.render(<App />, document.querySelector('.container'));
-
-  // axios
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-    .then(response => console.log(response))
-  ;
 
 });
